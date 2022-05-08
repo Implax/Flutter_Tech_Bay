a22acf0ef736b636feb13bcefae3e4dbb92a123d
Implax <juniourclottey@gmail.com>
2022-05-08 12:45:18 +0000

    "Finalised"

---
+++ lib/models/cart_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+import 'Prod';
+
+class CartController extends GetxController {
+  var _products = {}.obs;
+
+  void addProduct(Product product) {}
+}
---
+++ lib/screens/all_products/all_products.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AllProducts extends StatefulWidget {
+  const AllProducts({Key? key}) : super(key: key);
+  static String routeName = "/all_products";
+  @override
+  State<AllProducts> createState() => _AllProductsState();
+}
+
+class _AllProductsState extends State<AllProducts> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
